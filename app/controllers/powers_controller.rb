a08d7994d200e0085dcc4ac7095bd03e00910a01
yurubahmed <yurubiftin2002@gmail.com>
2023-03-26 13:26:12 -0400

    created routes

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,25 @@
 class PowersController < ApplicationController
+
+    def index
+        powers = Power.all
+        render json: powers
+    end
+
+    def show
+        power = Power.find(params[:id])
+        render json: power
+    end
+
+    def update
+        power = Power.find(params[:id])
+        if power
+            power.update(power_params)
+            render json: power, status: :accepted
+        else 
+            render json:{ error: "Power not found"}, status: :not found
+
+    private
+    def power_params
+        params.permit(:name, :description)
+    end
 end
